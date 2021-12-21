52272c3d4bb49831f743e24c40440465786be4b2
Jiahe <yi.jiahe00@gmail.com>
2021-12-21 11:37:31 +0800

    add ek docker to project

--- main.py
+++ main.py
@@ -59,8 +59,8 @@ if __name__ == '__main__':
 
             doc = prepare_doc_from_tweet(tweet)
 
-            # res = es.index(index="holo-tweets", id=tweet['data']['id'], document=doc)
-            # print(res['result'])
+            res = es.index(index="holo-tweets", id=tweet['data']['id'], document=doc)
+            print(res['result'])
 
         sleep_time = 30
         print(f"Disconnected from stream. Retrying in {sleep_time} seconds...")
