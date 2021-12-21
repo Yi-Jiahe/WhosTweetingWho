082fbdcc77edd9e3c6f58544d2eec29457175d4b
Jiahe <yi.jiahe00@gmail.com>
2021-12-21 22:39:58 +0800

    add visualization selector

--- visualizer/src/App.tsx
+++ visualizer/src/App.tsx
@@ -1,13 +1,43 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { ChangeEvent } from "react";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import './App.css';
 import { TweetGraph } from './TweetGraph';
+
+const routes = [
+  {
+    path: '/',
+    component: TweetGraph
+  }
+]
+
+const Header = (({ }) => {
+  let navigate = useNavigate();
+  let location = useLocation();
+
+  const onChange = (event: ChangeEvent<HTMLSelectElement>) => navigate(event.target.value);
+
+  return (
+    <header>
+      <select defaultValue={location.pathname} onChange={onChange}>
+        {routes.map((route) => (
+          <option value={route.path} key={route.path}>
+            {route.path === '/' ? 'Bubble pack' : route.path.substr(1, route.path.length)}
+          </option>
+        ))}
+      </select>
+    </header>
+  );
+});
 
 function App() {
   return (
     <div className="App height-100">
+      <Header />
       <Routes>
-        <Route path="/" element={<TweetGraph className="height-100 width-100" />} />
-        {/* <Route path="about" element={<About />} /> */}
+        {routes.map((route) => (
+          <Route path={route.path} element={<route.component className="height-100 width-100"/>} key={route.path} />
+        ))}
+        {/* <Route path="/" element={<TweetGraph className="height-100 width-100" />} /> */}
       </Routes>
     </div>
   );
